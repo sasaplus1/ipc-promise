9f7b4f02d36b2d0d6a802d4f69acf8820a85d3bf
Iwata Hidetaka <iwata0303@gmail.com>
2017-07-14 14:23:19 +0900

    Reproduction test: ipc-promise can't handle multiple calls.

---
+++ test/4/index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// NOTE: return test title when required by mocha
+if (typeof require('electron') === 'string') {
+  return (module.exports = 'can choose correct result when multiple calls at once');
+}
+
+const path = require('path'),
+      url = require('url');
+
+const electron = require('electron'),
+      app = electron.app,
+      BrowserWindow = electron.BrowserWindow;
+
+const ipcPromise = require('../../ipc-promise');
+
+let mainWindow;
+
+ipcPromise.on('delay_echo', function(params, event) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve(params);
+    }, 10);
+  });
+});
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({
+    show: false,
+  });
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+
+  const index = url.format({
+    protocol: 'file',
+    slashes: true,
+    pathname: path.join(__dirname, 'index.html'),
+  });
+
+  mainWindow.loadURL(index);
+});
